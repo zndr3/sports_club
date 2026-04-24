7eb05f6b3b1fa379604474b74410b9f2b3a9d475
zndr3 <zandersison18@gmail.com>
2026-04-24 16:35:47 +0800

    ADD: member, facility, booking lists, updates urls and views

--- management/urls.py
+++ management/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('facilities/', views.facilities, name='facilities'),
+    path('facility_list/', views.facility_list, name='facility_list'),
     path('booking_list/', views.booking_list, name='booking_list'),
+    path('member_list/', views.member_list, name='member_list'),
     
 ]
--- management/views.py
+++ management/views.py
@@ -1,22 +1,34 @@
+from multiprocessing import context
+from django.db.models import Count
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member, Facility, Booking
 
-def facilities(request):
+def facility_list(request):
   facilities = Facility.objects.all().values()
-  template = loader.get_template('facilities.html')
+  template = loader.get_template('facility_list.html')
   context = {
     'facilities': facilities,
   }
   return HttpResponse(template.render(context, request))
 
 def booking_list(request):
-  bookings = Booking.objects.select_related('facid', 'memid').all()
+
+  bookings = Booking.objects.values('facid__name').annotate(count_facid=Count('facid')).order_by('facid__name')
   template = loader.get_template('booking_list.html')
   context = {
     'bookings': bookings,
   }
   return HttpResponse(template.render(context, request))
+
+def member_list(request):
+  members = Member.objects.all().values()
+  template = loader.get_template('member_list.html')
+  context = {
+    'members': members,
+  }
+  return HttpResponse(template.render(context, request))
+
 
 def main(request):
   template = loader.get_template('main.html')
