3137551e24e19977d8446aecdbd3672c62f05718
zndr3 <zandersison18@gmail.com>
2026-05-01 23:00:30 +0800

    ADD: pagination in DRF results, updated base, allow scripts to fetch data from API URL

--- sports_club/settings.py
+++ sports_club/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'management',
 ]
 
@@ -125,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
